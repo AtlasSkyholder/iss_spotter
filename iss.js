25d9f8b5d2a2607908235e74053500847bb594af
Fred <thecanuckatheist@gmail.com>
2019-10-24 21:14:42 +0000

    code with error section

--- iss.js
+++ iss.js
@@ -13,6 +13,22 @@ const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
   let address = 'https://api.ipify.org?format=json';
   request(address, (error, response, body) => {
+
+    // inside the request callback ...
+    // error can be set if invalid domain, user is offline, etc.
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    // if we get here, all's well and we got the data
+
     let address = JSON.parse(body);
     callback(error, address['ip']);
   });
